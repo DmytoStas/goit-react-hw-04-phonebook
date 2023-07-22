58155b3214968c7628859bccc2ec3bebfb41466b
Dmytro Stas <dima7stas777@gmail.com>
2023-07-21 21:21:31 -0400

    refactoring the Phonebook task using hooks done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,42 +1,25 @@
 import { nanoid } from 'nanoid';
 
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 
 import Section from './Section';
 import ContactsList from './ContactsList';
 import Filter from './Filter';
-import PhonebookForm from './PhonebookForm/PhonebookForm';
+import PhonebookForm from './PhonebookForm';
 
 const LS_KEY = 'contacts';
 
-export default class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+function App() {
+  const [contacts, setContacts] = useState(() => {
+    return JSON.parse(window.localStorage.getItem(LS_KEY)) ?? [];
+  });
+  const [filter, setFilter] = useState('');
 
-  componentDidMount() {
-    const contacts = localStorage.getItem(LS_KEY);
-    const parseContacts = JSON.parse(contacts);
+  useEffect(() => {
+    window.localStorage.setItem(LS_KEY, JSON.stringify(contacts));
+  }, [contacts]);
 
-    if (parseContacts) {
-      this.setState({ contacts: parseContacts });
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    const newContacts = this.state.contacts;
-    const prevContacts = prevState.contacts;
-
-    if (newContacts !== prevContacts) {
-      localStorage.setItem(LS_KEY, JSON.stringify(newContacts));
-    }
-  }
-
-  handleSubmit = values => {
-    const { name, number } = values;
-    const { contacts } = this.state;
-
+  const handleSubmit = ({ name, number }) => {
     const hasContact = contacts.some(contact => {
       const normalizedName = contact.name.toLowerCase();
       return normalizedName.includes(name.toLowerCase());
@@ -49,51 +32,41 @@ export default class App extends Component {
         number,
       };
 
-      this.setState(prevState => ({
-        contacts: [...prevState.contacts, newContact],
-      }));
+      setContacts(prevContacts => [...prevContacts, newContact]);
     } else {
       alert(`${name} is already in contacts.`);
     }
   };
 
-  changeFilter = evt => {
-    this.setState({ filter: evt.target.value });
-  };
-
-  getFilteredContacts = () => {
-    const { filter, contacts } = this.state;
-    const normalizetFilter = filter.toLowerCase();
+  const visibleContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
 
     return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizetFilter)
+      contact.name.toLowerCase().includes(normalizedFilter)
     );
   };
 
-  deleteContact = evt => {
-    this.setState(prevState => {
-      const contactsAfterDelete = prevState.contacts.filter(
-        contact => evt.target.id !== contact.id
-      );
-      return { contacts: contactsAfterDelete };
-    });
+  const deleteContact = evt => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => evt.target.id !== contact.id)
+    );
   };
 
-  render() {
-    const { filter } = this.state;
-    const visibleContacts = this.getFilteredContacts();
-    return (
-      <>
-        <Section title={'Phonebook'}>
-          <PhonebookForm onSubmit={this.handleSubmit} />
-          <h2>Contacts</h2>
-          <Filter filter={filter} onChange={this.changeFilter} />
-          <ContactsList
-            contacts={visibleContacts}
-            onClick={this.deleteContact}
-          />
-        </Section>
-      </>
-    );
-  }
+  return (
+    <>
+      <Section title={'Phonebook'}>
+        <PhonebookForm onSubmit={handleSubmit} />
+        <h2>Contacts</h2>
+        <Filter
+          filter={filter}
+          onChange={e => {
+            setFilter(e.target.value);
+          }}
+        />
+        <ContactsList contacts={visibleContacts()} onClick={deleteContact} />
+      </Section>
+    </>
+  );
 }
+
+export default App;
--- src/components/PhonebookForm/PhonebookForm.jsx
+++ src/components/PhonebookForm/PhonebookForm.jsx
@@ -2,16 +2,12 @@ import PropTypes from 'prop-types';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
-import { Component } from 'react';
 import { FormContainer, Label, FormButton } from './PhonebookForm.styled';
 
-class PhonebookForm extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
+function PhonebookForm({ onSubmit }) {
+  const initialValues = { name: '', number: '' };
 
-  schema = Yup.object().shape({
+  const schema = Yup.object().shape({
     name: Yup.string()
       .required('Name required')
       .matches(
@@ -27,46 +23,42 @@ class PhonebookForm extends Component {
       ),
   });
 
-  static propTypes = {
-    onSubmit: PropTypes.func.isRequired,
-  };
-
-  handleSubmit = (values, actions) => {
-    this.props.onSubmit(values);
-
+  const handleSubmit = (values, actions) => {
+    onSubmit(values);
     actions.resetForm();
   };
 
-  render() {
-    return (
-      <Formik
-        initialValues={this.state}
-        validationSchema={this.schema}
-        onSubmit={this.handleSubmit}
-      >
-        <Form autoComplete="off">
-          <FormContainer>
-            <Label htmlFor="name">Name</Label>
-            <Field
-              type="text"
-              name="name"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            />
-            <ErrorMessage name="name" />
-
-            <Label htmlFor="number">Number</Label>
-            <Field
-              type="tel"
-              name="number"
-              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            />
-            <ErrorMessage name="number" />
-            <FormButton type="submit">Add contact</FormButton>
-          </FormContainer>
-        </Form>
-      </Formik>
-    );
-  }
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={schema}
+      onSubmit={handleSubmit}
+    >
+      <Form autoComplete="off">
+        <FormContainer>
+          <Label htmlFor="name">Name</Label>
+          <Field
+            type="text"
+            name="name"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          />
+          <ErrorMessage name="name" />
+          <Label htmlFor="number">Number</Label>
+          <Field
+            type="tel"
+            name="number"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          />
+          <ErrorMessage name="number" />
+          <FormButton type="submit">Add contact</FormButton>
+        </FormContainer>
+      </Form>
+    </Formik>
+  );
 }
+
+PhonebookForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
 
 export default PhonebookForm;
